73e4c5fbc5ae9dc690000c36d167d8974e8d8cf4
marnube89 <marnube89@gmail.com>
2025-03-10 08:52:12 +0100

    Corregido ejercicio de promesas de JS

--- JavaScript/Basicos/Promesas/ejer1.js
+++ JavaScript/Basicos/Promesas/ejer1.js
@@ -20,18 +20,20 @@ getData('https://swapi.dev/api/films', (result) => console.log(result), (error) 
 
 /**
  * async function getData(url, success, error) {
-    var req = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        var req = new XMLHttpRequest();
     req.open('GET', url); //
 
     req.onload = function() {
         if (req.status == 200) {
-            success(JSON.parse(req.response));
+            resolve(success(JSON.parse(req.response)));
         }
         else {
-            error();
+            reject(error());
         }
     };
         req.send();
+    })
 }
 
 getData('https://swapi.dev/api/films', (result) => console.log(result), (error) => console.log(error))
@@ -39,20 +41,21 @@ getData('https://swapi.dev/api/films', (result) => console.log(result), (error) 
 
 /**
  * async function getData(url, success, error) {
-    var req = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        var req = new XMLHttpRequest();
     req.open('GET', url); //
 
     req.onload = function() {
         if (req.status == 200) {
-            success(JSON.parse(req.response));
+            resolve(success(JSON.parse(req.response)));
         }
         else {
-            error();
+            reject(error());
         }
     };
         req.send();
+    })
 }
-
 
 getData('https://swapi.dev/api/films', (result) => console.log('Bien'), (error) => console.log('Mal'))
 getData('https://swapi.dev/api/people', (result) => console.log('Bien'), (error) => console.log('Mal'))
@@ -61,78 +64,84 @@ getData('https://swapi.dev/api/species', (result) => console.log('Bien'), (error
 
 /**
  * async function getData(url, success, error) {
-    var req = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        var req = new XMLHttpRequest();
     req.open('GET', url); //
 
     req.onload = function() {
         if (req.status == 200) {
-            success(JSON.parse(req.response));
+            resolve(success(JSON.parse(req.response)));
         }
         else {
-            error();
+            reject(error());
         }
     };
         req.send();
+    })
 }
 
 
-let prom1 = getData('https://swapi.dev/api/films', (result) => console.log('Bien'), (error) => console.log('Mal'))
+let prom1 = getData('https://swapi.dev/api/films', (result) => console.log('Bien1'), (error) => console.log('Mal'))
 
-let prom2 = getData('https://swapi.dev/api/people', (result) => console.log('Bien'), (error) => console.log('Mal'))
+let prom2 = getData('https://swapi.dev/api/people', (result) => console.log('Bien2'), (error) => console.log('Mal'))
 
-let prom3 = getData('https://swapi.dev/api/species', (result) => console.log('Bien'), (error) => console.log('Mal'))
+let prom3 = getData('https://swapi.dev/api/species', (result) => console.log('Bien3'), (error) => console.log('Mal'))
 
-Promise.all([prom1,prom2,prom3]).then(console.log('Resuelto'))
+await Promise.all([prom1,prom2,prom3]).then(result => console.log('Resuelto'))
  */
 
 /**
  * async function getData(url, success, error) {
-    var req = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        var req = new XMLHttpRequest();
     req.open('GET', url); //
 
     req.onload = function() {
         if (req.status == 200) {
-            success(JSON.parse(req.response));
+            resolve(success(JSON.parse(req.response)));
         }
         else {
-            error();
+            reject(error());
         }
     };
         req.send();
+    })
 }
 
 
-let prom1 = getData('https://swapi.dev/api/films', (result) => console.log('Bien'), (error) => console.log('Mal')).then(console.log('Primera'))
+let prom1 = getData('https://swapi.dev/api/films', (result) => result = 'promesa1', (error) => console.log('Mal'))
 
-let prom2 = getData('https://swapi.dev/api/people', (result) => console.log('Bien'), (error) => console.log('Mal'))
+let prom2 = getData('https://swapi.dev/api/people', (result) => result = 'promesa2', (error) => console.log('Mal'))
 
-let prom3 = getData('https://swapi.dev/api/species', (result) => console.log('Bien'), (error) => console.log('Mal'))
+let prom3 = getData('https://swapi.dev/api/species', (result) => result = 'promesa3', (error) => console.log('Mal'))
 
-Promise.all([prom1,prom2,prom3]).then(console.log('Resuelto'))
+await Promise.race([prom1,prom2,prom3]).then(result => console.log(result)).catch((error) => console.log('Fallo'))
  */
 
 /**
  * async function getData(url, success, error) {
-    var req = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        var req = new XMLHttpRequest();
     req.open('GET', url); //
 
     req.onload = function() {
         if (req.status == 200) {
-            success(JSON.parse(req.response));
+            resolve(success(JSON.parse(req.response)));
         }
         else {
-            error();
+            reject(error());
         }
     };
         req.send();
+    })
 }
 
 
-let prom1 = getData('https://swapi.dev/api/films', (result) => console.log('Bien'), (error) => console.log('Mal')).then(console.log('Primera'))
+let prom1 = getData('https://swapi.dev/api/films', (result) => console.log('Bien1'), (error) => console.log('Mal'))
 
-let prom2 = getData('https://swapi.dev/api/people', (result) => console.log('Bien'), (error) => console.log('Mal'))
+let prom2 = getData('https://swapi.dev/api/people', (result) => console.log('Bien2'), (error) => console.log('Mal'))
 
-let prom3 = getData('https://swapi.dev/api/species', (result) => console.log('Bien'), (error) => console.log('Mal'))
+let prom3 = getData('https://swapi.dev/api/species', (result) => console.log('Bien3'), (error) => console.log('Mal'))
 
 prom1.then(prom2).then(prom3).catch('Fallo')
  */
